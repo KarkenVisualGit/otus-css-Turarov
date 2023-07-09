d31423a5739c0ba0cc2db5ffe6efb31d74ca05db
Karken Turarov <karken_turarov@mail.ru>
2023-07-09 22:49:37 +0600

    added sharp plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCss = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -23,37 +23,6 @@ module.exports = {
         new MiniCss({
             filename:"style.css",
         }),
-        new ImageMinimizerPlugin({
-            test: /\.(png|jpe?g)$/i,
-            minimizer: {
-              filename: '[path][name][ext].webp',
-              implementation: ImageMinimizerPlugin.squooshMinify,
-              options: {
-                encodeOptions: {
-                  webp: {
-                    lossless: 1,
-                  },
-                },
-              },
-            },
-          }),
-          new ImageMinimizerPlugin({
-            test: /\.(png|jpe?g)$/i,
-            minimizer: {
-              implementation: ImageMinimizerPlugin.squooshMinify,
-              options: {
-                encodeOptions: {
-                  mozjpeg: {
-                    quality: 85,
-                  },
-                  oxipng: {
-                    level: 3,
-                    interlace: false,
-                  }
-                },
-              },
-            },
-          }),
     ],
     module: {
         rules: [
@@ -84,6 +53,26 @@ module.exports = {
     optimization: {
         minimizer: [
           new CssMinimizerPlugin(),
+          new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.sharpMinify,
+              options: {
+                encodeOptions: {
+                  jpeg: {
+                    quality: 100,
+                  },
+                  webp: {
+                    lossless: true,
+                  },
+                  avif: {
+                    lossless: true,
+                  },
+                  png: {},
+                  gif: {},
+                },
+              },
+            },
+          }),
         ],
       },
       
